e6f13e3d279d8a6b6b4b32c4f26d346ce39a567e
Louis Brosnan <louis@commonledger.com>
2017-10-04 14:20:18 +1300

    Add support for path mappings.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 npm i --save-dev pack-lambda
 ```
 
-### Usage
+### Setup
 
 package.json
 ```
@@ -25,4 +25,25 @@ package.json
 *
 !node_modules/**
 !index.js
+```
+
+### Run
+```
+npm run build
+```
+
+### Run with path mappings
+Moves any files inside the `build/` directories up to the root.
+```
+npm run build -m build/:
+```
+
+Moves any files inside the `build/` into a `dist/` directory.
+```
+npm run build -m build/:dist/
+```
+
+Multiple mappings.
+```
+npm run build -m build/:dist/ -m conf/:config/
 ```
--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const { spawn } = require('child_process');
 const tarToZip = require('tar-to-zip');
 const fs = require('fs');
 const { stdout } = process;
+const argv = require('minimist')(process.argv.slice(2));
 
 const PACKAGE_ROOT = 'package/';
+const DEFAULT_MAPPINGS = {
+    [PACKAGE_ROOT]: ''
+};
+
+const getMappings = argv => {
+    const mappingArgs = argv['m'] && (Array.isArray(argv['m']) ? argv['m'] : [argv['m']]) || [];
+    return mappingArgs.reduce((map, mapping) => {
+        const [from, to] = mapping.split(':');
+        map[from] = to;
+        return map;
+    }, DEFAULT_MAPPINGS);
+};
+
+const mappings = getMappings(argv);
 
 const pack = spawn('npm', ['pack']);
 
@@ -22,7 +37,7 @@ pack.on('close', _ => {
         filter: ({ name }) => name.substr(0, PACKAGE_ROOT.length) === PACKAGE_ROOT,
         map: ({ name }) => {
             return {
-                name: name.substr(PACKAGE_ROOT.length)
+                name: Object.entries(mappings).reduce((mapped, [from, to]) => mapped.replace(from, to), name)
             };
         }
     })
